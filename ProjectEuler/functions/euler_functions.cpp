6072aed55b128f98d7bdce10faf1f71caf455bae
Anikushin <Anikushin@rutoken.ru>
2015-12-03 14:33:30 +0300

    Task2

--- ProjectEuler/functions/euler_functions.cpp
+++ ProjectEuler/functions/euler_functions.cpp
@@ -24,9 +24,40 @@ void func1(size_t k1, size_t k2, size_t *pOut, size_t *ulOut, size_t x, ...)
                     pOut++;
                 }
 
-                *ulOut += 1;
+                (*ulOut)++;
                 break;
             }
         }
     }
+}
+
+void func2(size_t n1, size_t n2, size_t k, size_t *pOut, size_t *ulOut, bool start)
+{
+    if (start)
+    {
+        if (pOut)
+        {
+            pOut[0] = n1;
+            pOut[1] = n2;
+
+            pOut += 2;
+        }
+
+        *ulOut  = 2;
+    }
+
+    size_t current_value = n1 + n2;
+    
+    if (current_value >= k)
+        return;
+
+    if (pOut)
+    {
+        *pOut = current_value;
+        pOut++;
+    }
+
+    (*ulOut)++;
+
+    func2(n2, current_value, k, pOut, ulOut);
 }
