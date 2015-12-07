6e5aaa99912be286803c66a447bbd63aa0d3a0ab
Anikushin <Anikushin@rutoken.ru>
2015-12-07 18:10:12 +0300

    Task5

--- ProjectEuler/Task3/Task3.cpp
+++ ProjectEuler/Task3/Task3.cpp
@@ -3,6 +3,7 @@
 
 #include "euler_functions.h"
 
+
 // The prime factors of 13195 are 5, 7, 13 and 29.
 //
 // What is the largest prime factor of the number 600 851 475 143 ?
--- ProjectEuler/Task4/Task4.cpp
+++ ProjectEuler/Task4/Task4.cpp
@@ -4,9 +4,9 @@
 #include "euler_functions.h"
 
 
-//A palindromic number reads the same both ways.The largest palindrome made from the product of two 2 - digit numbers is 9009 = 91 × 99.
+// A palindromic number reads the same both ways.The largest palindrome made from the product of two 2 - digit numbers is 9009 = 91 × 99.
 //
-//Find the largest palindrome made from the product of two 3 - digit numbers.
+// Find the largest palindrome made from the product of two 3 - digit numbers.
 
 // TODO: think about optimization: https://projecteuler.net/thread=4&page=9#227033
 
