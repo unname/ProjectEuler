d47c7d7192096346772a8d724ebfad891122dec9
Anikushin <Anikushin@rutoken.ru>
2016-01-20 12:36:55 +0300

    Task43

---
+++ ProjectEuler/Task43/Task43.cpp
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <iostream>
+
+#include "euler_functions.h"
+
+
+// The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub - string divisibility property.
+//
+// Let d1 be the 1st digit, d2 be the 2nd digit, and so on.In this way, we note the following :
+//
+//      d2d3d4 = 406 is divisible by 2
+//      d3d4d5 = 063 is divisible by 3
+//      d4d5d6 = 635 is divisible by 5
+//      d5d6d7 = 357 is divisible by 7
+//      d6d7d8 = 572 is divisible by 11
+//      d7d8d9 = 728 is divisible by 13
+//      d8d9d10 = 289 is divisible by 17
+//
+// Find the sum of all 0 to 9 pandigital numbers with this property.
+
+int main(int argc, char **argv)
+{
+
+#ifdef _WIN64
+    size_t digits[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    size_t size = sizeof(digits) / sizeof(digits[0]);
+
+    size_t result = 0;
+
+    for (size_t serial_number = 0; serial_number < factorial(size); ++serial_number)
+    {
+        size_t n = get_lex_permut(digits, size, serial_number);
+        
+        size_t dividers[] = { 2, 3, 5, 7, 11, 13, 17 };
+
+        for (size_t i = 0; i < sizeof(dividers) / sizeof(dividers[0]); ++i)
+        {
+            if ((n % (size_t)pow(10, 9 - i) / (size_t)pow(10, 9 - 3 - i)) % dividers[i])
+                goto end;
+        }
+
+#ifdef _DEBUG
+        printf("%Iu\n", n);
+#endif
+        result += n;
+
+    end:;
+    }
+
+    printf("\nResult: %Iu\n", result);
+#endif
+
+    return 0;
+}
