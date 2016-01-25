7c84742651229f395148aedb8aab96aa5a93c6aa
Anikushin <Anikushin@rutoken.ru>
2016-01-25 11:04:48 +0300

    Task50 - update code

--- ProjectEuler/Task50/Task50.cpp
+++ ProjectEuler/Task50/Task50.cpp
@@ -42,28 +42,28 @@ int main(int argc, char **argv)
             max_sum_size = sum_size;
         }
 
+        //Проверяем все суммы начинающиеся не с 2
+        for (size_t i = sum_size - 1; i > max_sum_size; --i)
+        {
+            for (size_t j = 1;; ++j)
+            {
+                size_t current_sum = sum[i] - sum[i - max_sum_size - j];
+
+                if (current_sum > upper_bound)
+                    break;
+
+                if (test_prime(current_sum))
+                {
+                    max_sum = current_sum;
+                    max_sum_size = max_sum_size + j;
+                }
+            }
+        }
+
         //Дальше все последовательные суммы длинее max_sum_size будут привышать 1 миллион
         if (sum_size > max_sum_size)
             if (sum[sum_size - 1] - sum[sum_size - 1 - max_sum_size] > upper_bound)
                 break;
-    }
-
-    //Теперь нужно проверить все суммы начинающиеся не с 2
-    for (size_t i = sum_size - 1; i > max_sum_size; --i)
-    {
-        for (size_t j = 1; ; ++j)
-        {
-            size_t current_sum = sum[i] - sum[i - max_sum_size - j];
-
-            if (current_sum > upper_bound)
-                break;
-
-            if (test_prime(current_sum))
-            {
-                max_sum = current_sum;
-                max_sum_size = max_sum_size + j;
-            }
-        }
     }
 
     printf("\nResult(%Iu): %Iu\n", max_sum_size, max_sum);
